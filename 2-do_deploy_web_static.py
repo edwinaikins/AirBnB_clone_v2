58ddc24a9140fc43450f8fd230a53671cc10891e
edwinaikins <edwinaikins@gmail.com>
2022-09-30 03:35:15 +0000

    edited

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ import shlex
 import os
 
 
-env.hosts = ['35.231.33.237', '34.74.155.163']
+env.hosts = ['18.207.157.198', '3.238.129.82']
 env.user = "ubuntu"
 
 
@@ -36,5 +36,5 @@ def do_deploy(archive_path):
         run("ln -s {} /data/web_static/current".format(releases_path))
         print("New version deployed!")
         return True
-    except:
+    except RuntimeError:
         return False
